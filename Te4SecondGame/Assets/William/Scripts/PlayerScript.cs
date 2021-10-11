ad07940bc49a8bb78e95c9e60f2699c79f0a1538
Izze <william.isacsson@elev.ga.ntig.se>
2021-10-11 15:25:06 +0200

    Movable object
    Har lagt in ett script för att styra ett objekt och på börjat nätverks kod. Har bara gjort lite setup genom att drag n drop network script in i ett tomt objekt

--- Te4SecondGame/Assets/William/Scripts/PlayerScript.cs
+++ Te4SecondGame/Assets/William/Scripts/PlayerScript.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Vector3 movementX;
+    Vector3 movementZ;
+    Vector3 movementJump;
+    Rigidbody playerRigidBody;
+
+    float cooldown;
+    float cooldownCounter;
+
     void Start()
     {
-        
+        movementX = new Vector3(5, 0, 0);
+        movementZ = new Vector3(0, 0, 5);
+        movementJump = new Vector3(0, 5, 0);
+        playerRigidBody = GetComponent<Rigidbody>();
+
+        cooldownCounter = 2;
+        cooldown = 2;
     }
 
-    // Update is called once per frame
+    
+
     void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerRigidBody.AddForce(movementX * -1);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerRigidBody.AddForce(movementX);
+
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRigidBody.AddForce(movementZ * -1);
+            
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerRigidBody.AddForce(movementZ);
+
+        }
+
+        cooldownCounter += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space) && cooldownCounter >= cooldown)
+        {
+            cooldownCounter = 0;
+            playerRigidBody.AddForce(movementJump);
+
+        }
+
     }
 }
