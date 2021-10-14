111d826d7d4f002308f669a53d88e89dc8150572
Queen <ruben.bergenstrale@elev.ga.ntig.se>
2021-10-14 15:29:29 +0200

    gjorde fler animationer och är arg på kod
    Undrar hur jag ska göra med uppspelandet av animationer, än så länge funkar det mesta...

--- Te4SecondGame/Assets/Ruben/ScriptsR/BonkMovement.cs
+++ Te4SecondGame/Assets/Ruben/ScriptsR/BonkMovement.cs
@@ -4,18 +4,44 @@ using UnityEngine;
 
 public class BonkMovement : MonoBehaviour
 {
-
+    public float startingTime = 0f;
+    public float currentTime = 0.5f;
     Animator animate;
+    public string currentState;
+
+    public const string PLAYER_HEAVY = "StopBonkHeavy";
+    //public const string PLAYER_LIGHT = "StopBonk";
+
+    void ChangeAnimationState(string newState)
+    {
+
+        //stop the same animation from interrupting itself.
+        if (currentState == newState)
+        {
+            return;
+        }
+
+        //play animation.
+        animate.Play(newState);
+
+        //reassign the current state.
+        currentState = newState;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         animate = GetComponent<Animator>();
+        currentTime = startingTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetButtonDown("Fire3") /* && isInteracting = false*/)
+        //{
+        //    ChangeAnimationState(PLAYER_LIGHT);
+        //}
         if (Input.GetButtonDown("Fire1"))
         {
             animate.SetBool("attacking", true);
@@ -23,6 +49,24 @@ public class BonkMovement : MonoBehaviour
         else if (Input.GetButtonUp("Fire1"))
         {
             animate.SetBool("attacking", false);
+            //if (isInteracting = false)
+            //{
+            //  currentTime += 0.5f * Time.deltaTime;
+            //}
+        }
+        // when animation end, initiate below.
+
+
+        if (currentTime < 0.5f * Time.deltaTime)
+        {
+            //initate animation 2
+        }
+
+
+        if (Input.GetButtonDown("Fire3") /* && isInteracting = false*/)
+        {
+            ChangeAnimationState(PLAYER_HEAVY);
+            
         }
     }
 }
