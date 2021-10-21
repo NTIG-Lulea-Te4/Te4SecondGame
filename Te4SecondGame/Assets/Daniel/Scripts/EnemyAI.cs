22fc822cd1a4bca41ad9655efbf459b9cfeddf1a
Daniel3178 <89932502+Daniel3178@users.noreply.github.com>
2021-10-21 12:17:07 +0200

    Done with AI-enemi
    Animation, movement, AI are done!

--- Te4SecondGame/Assets/Daniel/Scripts/EnemyAI.cs
+++ Te4SecondGame/Assets/Daniel/Scripts/EnemyAI.cs
@@ -8,26 +8,34 @@ public class EnemyAI : MonoBehaviour
     public float lookRadius = 10f;
     Transform target;
     NavMeshAgent agent;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Vector3 direction = transform.DirectionTo(target.position);
         float distance = Vector3.Distance(target.position, transform.position);
         if (distance <= lookRadius)
         {
-            agent.SetDestination(target.position);
-
-            if(distance <= agent.stoppingDistance)
+            if (distance <= agent.stoppingDistance)
             {
                 FaceTarget();
+                
             }
+            agent.SetDestination(target.position);
+            animator.SetFloat("Speed", agent.velocity.magnitude);
+            //LookToward(transform.position, distance);
+            //Vector3 movement = transform.forward * Time.deltaTime * 1f;
         }
+        animator.SetFloat("Speed", agent.velocity.magnitude);
     }
 
     void FaceTarget()
@@ -35,6 +43,7 @@ public class EnemyAI : MonoBehaviour
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        animator.SetFloat("Speed", 0f);
     }
 
     private void OnDrawGizmosSelected()
